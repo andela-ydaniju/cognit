b9b3e1a2f4ac4c90054daf76e77756697ea2a0da
Tijesunimi Peters <tijesunimi.peters@andela.com>
2016-09-03 13:42:13 +0100

    update

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -10,6 +10,28 @@ class LandingController < ApplicationController
   end
 
   def show
-    @personality = session[:p_data]
+  end
+
+  def display
+    # @personality = session[:p_data]
+    # @values = session[:v_data]
+    # @needs = session[:n_data]
+
+    pb = File.read("config/problem.json")
+    cred = {
+    url: "https://gateway.watsonplatform.net/tradeoff-analytics/api",
+    password: "T41CufPA1r8s",
+    username: "15a89132-602f-429d-a2b2-83f54102f281"
+}
+
+    @res = JSON.parse Excon.post("#{cred[:url]}/v1/dilemmas?generate_visualization=true",
+                                 body: pb,
+                                 headers: {"Content-Type": "application/json"},
+                                 user: cred[:username],
+                                 password: cred[:password]
+                                ).body
+
+    binding.pry
+
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class SessionsController < ApplicationController
   before_action :client, only: [:create]
   before_action :access_token, only: [:create]
@@ -52,7 +53,7 @@ class SessionsController < ApplicationController
       }
     end
 
-    session[:p_data] = @p_data.to_json
+    session[:p_data] = @p_data
     session[:n_data] = @n_data.to_json
     session[:v_data] = @v_data.to_json
 
@@ -60,6 +61,65 @@ class SessionsController < ApplicationController
   end
 
   def process_insights
-    redirect_to root_path
+    @personality = session[:p_data]
+    @values = session[:v_data]
+    @needs = session[:n_data]
+    # pb = File.read("config/problem.json")
+    cred = {
+    url: "https://gateway.watsonplatform.net/tradeoff-analytics/api",
+    password: "T41CufPA1r8s",
+    username: "15a89132-602f-429d-a2b2-83f54102f281"
+}
+max_or_min = ["min", "max"]
+names = @personality[0][:children].map {|e|
+  {
+    key: e[:name],
+    type: "numeric",
+    goal: max_or_min.sample,
+    is_objective: true,
+    full_name: e[:name].capitalize,
+    range: {
+      "low": 0,
+      "high": 1
+    }
+  }
+}
+
+
+arr = []
+personality = []
+value = []
+needs = []
+count = 0
+CSV.foreach('companylist-1.csv') do |row|
+  arr << {
+    key: count += 1,
+    name: row[6],
+    values: {
+      Openness: rand(0..1.0),
+      Conscientiousness: rand(0..1.0),
+      Extraversion: rand(0..1.0),
+      Agreeableness: rand(0..1.0),
+      "Emotional range": rand(0..1.0)
+    }
+  }
+end
+
+
+pb = {
+  subject: "finance",
+  columns: names,
+  options: arr
+}
+
+
+    @res = JSON.parse Excon.post("#{cred[:url]}/v1/dilemmas?generate_visualization=false",
+                                 body: pb.to_json,
+                                 headers: {"Content-Type": "application/json"},
+                                 user: cred[:username],
+                                 password: cred[:password]
+                                ).body
+    @companies = @res["resolution"]["solutions"].select{|d| d["status"] == "FRONT"}
+    render json: @companies
   end
 end
