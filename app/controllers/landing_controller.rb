6a70ab8a66bcf211decff4c24b9b6c77f12d867a
andela-ydaniju <yusuf.daniju@andela.com>
2016-09-03 14:39:04 +0100

    finsih error with domain

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,7 +2,8 @@ class LandingController < ApplicationController
   before_action :client
 
   def index
-    request_token = @client.request_token oauth_callback: "http://127.0.0.1:3000/auth/token/callback"
+    request_token = @client.request_token oauth_callback: "#{request.env['REQUEST_URI']}auth/token/callback"
+
     session[:oauth] = request_token
     session[:oauth_url] = request_token.authorize_url
     @authorize_url = request_token.authorize_url
