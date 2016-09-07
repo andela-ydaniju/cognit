9044ebc53d8c57f45163141dec5ee7d41f9e6413
andela-ydaniju <yusuf.daniju@andela.com>
2016-09-07 23:52:27 +0100

    add password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@
 #
 # Table name: users
 #
-#  id            :integer          not null, primary key
-#  name          :string
-#  email         :string
-#  password      :string
-#  acount_number :integer
-#  atm_pin       :integer
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string
+#  email           :string
+#  password_digest :string
+#  acount_number   :integer
+#  atm_pin         :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 
 class User < ActiveRecord::Base
